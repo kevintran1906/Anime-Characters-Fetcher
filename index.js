c69d1392c0921c7d1158e814dac45f84eb41f3b8
Kevin Tran <kevintran19062007@gmail.com>
2024-11-19 09:31:09 -0500

    update code for index js. Sprite can be shown now.

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ async function fetchData(){
         }
         
         const data = await response.json();
-        console.log(data);
+        const pokemonSprite = data.sprites.front_default;
+        const imgElement = document.getElementById("pokemonSprite");
+
+        imgElement.src = pokemonSprite;
+        imgElement.style.display = "block";
 
     }
     catch(error){
