ee1ac0bec016ce07def1c8616e55649592a4c9a7
Kevin Tran <kevintran19062007@gmail.com>
2024-11-19 22:11:37 -0500

    delete test code

--- index.js
+++ index.js
@@ -1,54 +1,27 @@
-//fetchData();
-
-// async function fetchData(){
-
-//     try{
-
-//         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-//         const response = await fetch("https://pokeapi.co/api/v2/pokemon/${pokemonName}")
-
-//         if(!response.ok){
-//             throw new Error('Could not fetch resource')
-
-//         }
-        
-//         const data = await response.json();
-//         //console.log(data);
-//         const pokemonSprite = data.sprites.front_default;
-//         const imgElement = document.getElementById("pokemonSprite");
-
-//         imgElement.src = pokemonSprite;
-//         imgElement.style.display = "block";
-
-//     }
-//     catch(error){
-//         console.error(error);
-
-//     }
-
-// }
-
-
 async function fetchData(){
 
     try{
 
         const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
 
         if(!response.ok){
-            throw new Error("Could not fetch resource");
-        }
+            throw new Error('Could not fetch resource')
 
+        }
+        
         const data = await response.json();
-        console.log(data)
+        console.log(data);
         const pokemonSprite = data.sprites.front_default;
         const imgElement = document.getElementById("pokemonSprite");
 
         imgElement.src = pokemonSprite;
         imgElement.style.display = "block";
+
     }
     catch(error){
         console.error(error);
+
     }
+
 }
